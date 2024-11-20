78ee47d9ba0a3bd467d3070320bf1c33bd3c7512
zsidanyi <zsidanyi@gmail.com>
2024-11-20 12:51:06 +0100

    Font setting done

--- os/install/01_install.sh
+++ os/install/01_install.sh
@@ -10,12 +10,23 @@ fi
 
 systemctl enable dhcpcd.service
 
-for pkg_file in $(dirname "$0")/pkg_files/01*; do
+if ! [[ -d $1 ]]; then
+  fatal "Not a proper dir is provided"
+fi
+
+pkgfiles_path=$(realpath -s "$1")
+echo $pkgfiles_path
+
+for pkg_file in $pkgfiles_path/*; do
   info "Installing packages: "
-  cat $pkg_file | tr '\n' ' '
+  cat $pkg_file | tr '\n' ' '; echo ""
   # Installing the packages
   if ask "Install `basename $pkg_file`?"; then
     pacman -S --needed --noconfirm - < $pkg_file
   fi
 done
 
+# mkdir -p ~/.local/share/fonts/ttf/Cousine
+# unzip ~/dotfiles/lfs_artifacts/terminal_font/Cousine.zip -d ~/.local/share/fonts/ttf/Cousine
+# chmod 444 ~/.local/share/fonts/ttf/Cousine/*
+# fc-cache
